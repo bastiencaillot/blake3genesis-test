b253985cdbb03bcd1d336f9f1b68f04cc0763d8f
Bastien Caillot <83666556+bastiencaillot@users.noreply.github.com>
2021-08-31 23:07:50 +0200

    Create genesis.js

---
+++ genesis.js
@@ -0,0 +1,172 @@
+
+
+var $ = require("./utils");
+
+var defaults = {
+    //the (unix) time when the genesisblock is created
+    time: Math.round((new Date()).getTime() / 1000),
+    //the pszTimestamp found in the coinbase of the genesisblock
+    timestamp: "CNBC Aug 24 2021 Chinese tech stocks rally as JD.com and Alibaba surge nearly 9%",
+    //the first value of the nonce that will be incremented when searching the genesis hash
+    nonce: 1,
+    //the PoW algorithm: Blake3
+    algorithm: 'Blake3',
+    //the pubkey found in the output script
+    pubkey: '040184710fa689ad5023690c80f3a49c8f13f8d45b8c857fbcbc8bc4a8e4d3eb4b10f4d4604fa08dce601aaf0f470216fe1b51850b4acf21b179c45070ac7b03a9',
+    //the value in coins for the output, full value (exp. in bitcoin 5000000000 - To get other coins value: Block Value * 100000000)
+    value: 5000000000,
+    //the target in compact representation, associated to a difficulty of 1
+    bits: 0x1e0ffff0,
+    locktime: 0
+}
+
+require('yargs')
+    .alias('t', 'time')
+    .alias('z', 'timestamp')
+    .alias('n', 'nonce')
+    .alias('a', 'algorithm')
+    .alias('p', 'pubkey')
+    .alias('v', 'value')
+    .alias('b', 'bits')
+    .alias('l', 'locktime')
+    .alias('h', 'help')
+    .help()
+    .command('*', 'create genesis block', () => { }, (argv) => {
+        // console.log(argv);
+
+        var options = Object.assign({}, defaults, argv);
+        // console.log(options);
+
+        var merkle_root = $.blake3d_hash(createTx(options));
+        var genesisblock = createBlock(merkle_root, options);
+        console.log("---------------");
+        console.log("algorithm: %s", options.algorithm);
+        console.log("pzTimestamp: %s", options.timestamp);
+        console.log("pubkey: %s", options.pubkey);
+        console.log("bits: %s", options.bits);
+        console.log("time: %s", options.time);
+        console.log("merkle root hash: %s", $.reverseBuffer(merkle_root).toString('hex'));
+
+        PoW(genesisblock, options);
+
+        //console.log("genesis block: %s", genesisblock.toString('hex'));
+        // console.log($.reverseBuffer(hash_merkle_root).toString('hex'));
+
+    })
+    .argv;
+
+
+function createInputScript(options) {
+    var tz = options.timestamp;
+    var psz_prefix = tz.length > 76 ? '4c' : '';
+    var script_prefix = '04ffff001d0104' + psz_prefix + Buffer.from(String.fromCharCode(tz.length)).toString('hex');
+    return Buffer.from(script_prefix + Buffer.from(tz).toString('hex'), 'hex');
+}
+
+function createOutputScript(options) {
+    return Buffer.from('41' + options.pubkey + 'ac', 'hex');
+}
+
+
+function createTx(options) {
+
+
+    var input = createInputScript(options);
+    var out = createOutputScript(options);
+   
+    var size = 4    // tx version
+        + 1   // number of inputs
+        + 32  // hash of previous output
+        + 4   // previous output's index
+        + 1   // 1 byte for the size of scriptSig
+        + input.length
+        + 4   // size of sequence
+        + 1   // number of outputs
+        + 8   // 8 bytes for coin value
+        + 1   // 1 byte to represent size of the pubkey Script
+        + out.length
+        + 4;   // 4 bytes for lock time
+
+
+    var tx = Buffer.alloc(size);
+
+    var position = 0;
+
+    tx.writeInt32LE(1, position);
+    tx.writeInt32LE(1, position += 4);
+    tx.write(Buffer.alloc(32).toString('hex'), position += 1, 32, 'hex');
+    //tx.writeInt32LE(0xFFFFFFFF, position += 32, 4);
+    tx.writeDoubleLE(0xFFFFFFFF, position += 32, 4);
+    tx.writeInt32LE(input.length, position += 4);
+    tx.write(input.toString('hex'), position += 1, input.length, "hex");
+    //tx.writeInt32LE(0xFFFFFFFF, position += input.length, 4);
+    tx.writeDoubleLE(0xFFFFFFFF, position += input.length);
+    tx.writeInt32LE(1, position += 4);
+    tx.write(Buffer.from($.numToBytes(options.value)).toString('hex'), position += 1, 8, 'hex'); // 50 * coin
+    tx.writeInt32LE(0x43, position += 8);
+    //tx.write(input.toString('hex'), position += 1, input.length, "hex");
+    tx.write(out.toString('hex'), position += 1, out.length, "hex");
+    tx.writeInt32LE(options.locktime, position += out.length);
+
+
+    // console.log(tx.toString('hex'));
+
+    return tx;
+
+
+
+};
+
+
+function createBlock(merkleRoot, options) {
+
+    var block = Buffer.alloc(80);
+    var position = 0;
+
+    block.writeInt32LE(1, position); //version  
+    block.write(Buffer.alloc(32).toString('hex'), position += 4, 32, 'hex'); //previousblockhash
+    block.write(merkleRoot.toString('hex'), position += 32, 32, 'hex');
+    block.writeInt32LE(options.time, position += 32);
+    //block.write(Buffer.from($.numToBytes(options.time)).toString('hex'), position += 32, 4, 'hex');
+
+    block.writeInt32LE(options.bits, position += 4);
+    //block.write(Buffer.from($.numToBytes(options.bits)).toString('hex'), position += 4, 4, 'hex');
+
+    block.writeInt32LE(options.nonce, position += 4);
+
+    return block;
+
+};
+
+function PoW(data, options) {
+    
+    var blake3 = require('blake3');
+
+    console.log('Searching for genesis hash...');
+    var nonce = options.nonce;
+
+    //var target = $.numToBytes((options.bits & 0xffffff) * 2 ** (8 * ((options.bits >> 24) - 3)));
+    // console.log('' + target.toString('hex'));    
+
+    while (true) {
+
+        var hash = $.reverseBuffer(blake3.hash(data)).toString('hex');
+        // var hash2 = $.reverseBuffer(hash).toString('hex');
+        // var val = parseInt(hash2, 16);
+
+        if (hash.match(/^00000/)) {
+            console.log("nonce: %s", nonce);
+            console.log("genesis hash: %s", hash);
+            return;
+        } else {
+
+            nonce += 1;
+            // if (nonce % 2000 == 0) {
+            //     console.log("nonce: %s | hash: %s ", nonce, hash.toString('hex'));
+            // }
+            data.writeInt32LE(nonce, data.length - 4);
+        }
+
+    }
+
+}
